6717c852de1b8bbb99e23a92ff698d9afe3fdd99
Matheus Selis Soares <87715995+udoremi@users.noreply.github.com>
2024-05-13 16:49:28 -0300

    at

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,18 +1,21 @@
 function carregar(){
+    
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora >= 0 && hora <= 12){
-        img.src = 'fotomanha.png'
-    }
-    else{
-        if(hora >= 12 && hora <= 18){
-            img.src = 'fototarde.png'
-        }
-        else{
-            img.src = 'fotonoite.png'
-        }
+    var min = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if(hora >= 12 && hora <= 18){
+        img.src = 'fototarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else{
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = '#515154'
     }
 }
