365cd8bf23880352d4e08cbc98ce5608ae8efadd
Matheus Selis Soares <87715995+udoremi@users.noreply.github.com>
2024-05-14 16:02:38 -0300

    novo ex

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert("Olá")
+function tabuada(){
+
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número')
+    } else{
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+}
